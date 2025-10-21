b093d09ea098ce832782828340cf1584c79aab37
leminhbac2001 <leminhbac2001111@gmail.com>
2025-10-21 23:37:43 +0700

    add project okrs

--- src/@core/db/projects.ts
+++ src/@core/db/projects.ts
@@ -1,56 +1,64 @@
 const projectsDb = [
-{
-    title: "Website rent Hotel",
-    content: "Hệ thống đặt phòng khách sạn (FrontEnd)",
-    brief: "HTMl CSS Javascript Boostrap JQuery",
-    img: require('../../resources/imgs/projects/rent.png'),
-    id: 7,
-    source: "https://github.com/minhbac01job/Website-Rent-Hotel",
-    href: "https://minhbac01job.github.io/Website-Rent-Hotel/"
-},
-{
-    title: "Messenger Clone",
-    content: "Ứng dụng lấy ý tưởng từ Messager của Facebook, với mục dích trao đổi thông tin giữa nhiều người dùng",
-    brief: "HTML SCSS NextJS ReactJS tailwind JavaScript TypeScript",
-    img: require('../../resources/imgs/projects/messenger.png'),
-    id: 2,
-    href: "https://messenger-clone-pi-azure.vercel.app/",
-    source: "https://github.com/minhbac01job/messenger-clone",
-},
-{
-    title: "Zing MP3",
-    content: "Website nghe nhạc trực tuyến",
-    brief: "HTML SCSS NextJS ReactJS tailwind JavaScript",
-    img: require('../../resources/imgs/projects/zing.png'),
-    id: 3,
-    href: "https://zing-mp3-phi.vercel.app/",
-    source: "https://github.com/minhbac01job/zing-mp3"
-},
-{
-    title: "Tiktok Clone",
-    content: "Website lấy ý tưởng từ Tiktok 1 mạng xã hội cực kì phổ biến",
-    brief: "HTML SCSS NextJS ReactJS tailwind JavaScript NodeJS MongoDB TypeScript",
-    img: require('../../resources/imgs/projects/tiktok.png'),
-    id: 4,
-    href: "https://tiktok-clone-ebon.vercel.app/",
-    source: "https://github.com/minhbac01job/tiktok-clone"
-},
-{
-    title: "KhoaBang Acedemy",
-    content: "Hệ thống quản lý sinh viên của trung tâm Khoa Bảng",
-    brief: "Android Java NodeJS MYSQL",
-    img: require('../../resources/imgs/projects/cherfnodes.jpg'),
-    id: 5,
-    source: "https://github.com/minhbac01job/Scientific"
-},
-{
-    title: "Template app mobile ReactNative",
-    content: "Template app mobile React Native hỗ trợ việc phát triển mới",
-    brief: "Javascript ReactNative Typescript",
-    img: require('../../resources/imgs/projects/cherfnodes.jpg'),
-    id: 6,
-    source: "https://github.com/minhbac01job/ReactNativeTemplateBaseTS"
-},
+    {
+        title: "BoostGo HRMS",
+        content: "Hệ thống quản lý nhân sự, quản lý tài chính BoostGo",
+        brief: "Javascript, NodeJs, JQuery, EJS",
+        img: require('../../resources/imgs/projects/bg-okrs.png'),
+        id: 1,
+        href: "https://okrs-lnzm.onrender.com/"
+    },
+    {
+        title: "Website rent Hotel",
+        content: "Hệ thống đặt phòng khách sạn (FrontEnd)",
+        brief: "HTMl CSS Javascript Boostrap JQuery",
+        img: require('../../resources/imgs/projects/rent.png'),
+        id: 7,
+        source: "https://github.com/minhbac01job/Website-Rent-Hotel",
+        href: "https://minhbac01job.github.io/Website-Rent-Hotel/"
+    },
+    {
+        title: "Messenger Clone",
+        content: "Ứng dụng lấy ý tưởng từ Messager của Facebook, với mục dích trao đổi thông tin giữa nhiều người dùng",
+        brief: "HTML SCSS NextJS ReactJS tailwind JavaScript TypeScript",
+        img: require('../../resources/imgs/projects/messenger.png'),
+        id: 2,
+        href: "https://messenger-clone-pi-azure.vercel.app/",
+        source: "https://github.com/minhbac01job/messenger-clone",
+    },
+    {
+        title: "Zing MP3",
+        content: "Website nghe nhạc trực tuyến",
+        brief: "HTML SCSS NextJS ReactJS tailwind JavaScript",
+        img: require('../../resources/imgs/projects/zing.png'),
+        id: 3,
+        href: "https://zing-mp3-phi.vercel.app/",
+        source: "https://github.com/minhbac01job/zing-mp3"
+    },
+    {
+        title: "Tiktok Clone",
+        content: "Website lấy ý tưởng từ Tiktok 1 mạng xã hội cực kì phổ biến",
+        brief: "HTML SCSS NextJS ReactJS tailwind JavaScript NodeJS MongoDB TypeScript",
+        img: require('../../resources/imgs/projects/tiktok.png'),
+        id: 4,
+        href: "https://tiktok-clone-ebon.vercel.app/",
+        source: "https://github.com/minhbac01job/tiktok-clone"
+    },
+    {
+        title: "KhoaBang Acedemy",
+        content: "Hệ thống quản lý sinh viên của trung tâm Khoa Bảng",
+        brief: "Android Java NodeJS MYSQL",
+        img: require('../../resources/imgs/projects/cherfnodes.jpg'),
+        id: 5,
+        source: "https://github.com/minhbac01job/Scientific"
+    },
+    {
+        title: "Template app mobile ReactNative",
+        content: "Template app mobile React Native hỗ trợ việc phát triển mới",
+        brief: "Javascript ReactNative Typescript",
+        img: require('../../resources/imgs/projects/cherfnodes.jpg'),
+        id: 6,
+        source: "https://github.com/minhbac01job/ReactNativeTemplateBaseTS"
+    },
 ]
 export {
     projectsDb
